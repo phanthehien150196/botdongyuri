623ec484c511e2982bf187fc74060b7174a582a9
phanthehien150196 <phanthehien150196@gmail.com>
2020-07-11 21:55:33 +0700

    First

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");	
+const axios = require("axios");		
+var bot = new Discord.Client();		
+bot.on('ready', function(){		  
+	console.log("bot is now online");		
+})		
+bot.on("message", async message => {
+	if(message.content.indexOf(".") === 0){
+	axios.get('https://simsumi.herokuapp.com/api?text='+encodeURI(message.content.slice(1).trim())+'&lang=vi')
+      .then( response =>{
+        message.channel.send(response.data.success);
+      } )
+
+	//	message.channel.send("pong");		  
+	}		
+})		
+bot.login(process.env.token);
