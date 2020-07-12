ccab51ed7ec35214857edc3454c4703b90277702
phanthehien150196 <phanthehien150196@gmail.com>
2020-07-12 10:47:22 +0700

    update

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ bot.on('ready', function(){
 	console.log("bot is now online");		
 })		
 bot.on("message", async message => {
-	if(message.content.indexOf(".") === 0){ console.log(message.content.slice(1).trim())
+	if(message.content.toLowerCase().indexOf("avatar")!=-1&&getUserFromMention(message.content)!=false){
+		message.channel.send(`${user.username} có avatar là: ${user.displayAvatarURL({ dynamic: true })}`)
+	}
+	else if(message.content.indexOf(".") === 0){ console.log(message.content.slice(1).trim())
 	axios.get('https://simsumi.herokuapp.com/api?text='+encodeURI(message.content.slice(1).trim())+'&lang=vi')
       .then( response =>{
       	if(response.data.success=="") {
@@ -18,5 +21,18 @@ bot.on("message", async message => {
 
 	//	message.channel.send("pong");		  
 	}		
-})		
+})
+function getUserFromMention(mention) {
+	// The id is the first and only match found by the RegEx.
+	const matches = mention.match(/^<@!?(\d+)>$/);
+
+	// If supplied variable was not a mention, matches will be null instead of an array.
+	if (!matches) return;
+
+	// However the first element in the matches array will be the entire mention, not just the ID,
+	// so use index 1.
+	const id = matches[1];
+
+	return client.users.cache.get(id);
+}		
 bot.login(process.env.token);
