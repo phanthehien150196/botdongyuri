3b0e30bb6e6b3a91843aca2d1871bd805e314784
phanthehien150196 <phanthehien150196@gmail.com>
2020-07-12 10:27:37 +0700

    update

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ bot.on('ready', function(){
 	console.log("bot is now online");		
 })		
 bot.on("message", async message => {
-	if(message.content.indexOf(".") === 0){
+	if(message.content.indexOf(".") === 0){ console.log(message.content.slice(1).trim())
 	axios.get('https://simsumi.herokuapp.com/api?text='+encodeURI(message.content.slice(1).trim())+'&lang=vi')
       .then( response =>{
       	if(response.data.success=="") {
-      		message.channel.send("Không hiểu");
+      		message.channel.send("<@"+message.author +"> Không hiểu");
       	}
-      	else
-        message.channel.send("<@"+message.author +"> "+response.data.success);
+      	else{
+      		message.channel.send("<@"+message.author +"> "+response.data.success);
+      	}
       } )
 
 	//	message.channel.send("pong");		  
