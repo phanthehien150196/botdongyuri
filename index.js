bd3f748c4b03c2e708cccd3f83760d66eb44e435
phanthehien150196 <phanthehien150196@gmail.com>
2020-07-12 10:58:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ bot.on('ready', function(){
 })		
 bot.on("message", async message => {
 	if(message.content.toLowerCase().indexOf("avatar")!=-1&&getUserFromMention(message.content)!=false){
-		user=getUserFromMention(message.content)
+		const user=getUserFromMention(message.content)
 		console.log(user)
 		message.channel.send(`${user.username} có avatar là: ${user.displayAvatarURL({ dynamic: true })}`)
 	}
