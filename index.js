cd12017d11fc1b8646ec44b01d9a5351d4f4934e
phanthehien150196 <phanthehien150196@gmail.com>
2020-07-18 14:46:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.on("message", async message => {
 	if(message.content.toLowerCase().indexOf(".th")!=-1&&getUserFromMention(message.content)!=false)
 	{
 		let user = message.mentions.users.first();
-		message.channel.send(user.createdAt.toLocaleString('vi', { timeZone: 'Asia/Ho_Chi_Minh' }));
+		message.channel.send(user.createdAt.getDay().toLocaleString('vi', { timeZone: 'Asia/Ho_Chi_Minh' }));
 	}
 	else if(message.content.toLowerCase().indexOf("avatar")!=-1&&getUserFromMention(message.content)!=false){
 		let user = message.mentions.users.first();
