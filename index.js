2eada1aba638f188f2b4cd14ec30a44d612d2173
phanthehien150196 <phanthehien150196@gmail.com>
2020-07-26 01:56:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.on("message", async message => {
 		let user = message.mentions.users.first();
 		message.channel.send(user.username+" có avatar là "+ user.displayAvatarURL({ dynamic:true,format:"png",size:4096 }))
 	}
-	else if(getGame(change_alias(message.content)) !=false&&message.author.id!="574602677929902080"&&message.channel.id =="533170013129932801"&&message.member.roles.cache.some(r => r.name === "Admin")==false){
+	else if(getGame(change_alias(message.content)) !=false&&message.author.id!="574602677929902080"&&message.channel.id =="533170013129932801"&&message.member.roles.cache.some(r => r.name === "Mod")==false){
 		message.delete({ timeout: 1 });
 	}
 	else if(message.content.indexOf(".") === 0){ console.log(message.content.slice(1).trim())
