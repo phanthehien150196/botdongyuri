0a9563df40aed0a3ae668595209437ec3319cedf
phanthehien150196 <phanthehien150196@gmail.com>
2020-07-26 15:45:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -59,9 +59,6 @@ function getGame(mention) {
 function change_alias(alias) {
     var str = alias;
     str = str.toLowerCase();
-    str = str.replace("google","")
-    str = str.replace("gende","")
-    str = str.replace("ghe","")
     str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g,"a"); 
     str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g,"e"); 
     str = str.replace(/ì|í|ị|ỉ|ĩ/g,"i"); 
@@ -71,6 +68,11 @@ function change_alias(alias) {
     str = str.replace(/đ/g,"d");
     str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g," ");
     str = str.replace(/ + /g," ");
+    str = str.replace("ghie","")
+    str = str.replace("google","")
+    str = str.replace("gende","")
+    str = str.replace("ghe","")
+    
     str = str.trim(); 
     return str;
 }
