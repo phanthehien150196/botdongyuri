c87a6d7cd6394c20bcef12b4a962298d341f8ea8
phanthehien150196 <phanthehien150196@gmail.com>
2020-07-29 02:56:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ bot.on("message", async message => {
 		await bot.channels.cache.get(`543459440691642408`).send("<@"+message.author +"> Nếu bạn đang nói về game thì xin hãy thảo luận ở phòng này")
 
 	}
+	else if(message.content.indexOf("$") === 0&&message.content.toLowerCase().indexOf("xp") != -1&&message.channel.id =="533170013129932801"){
+		await message.delete({ timeout: 1 });
+		await bot.channels.cache.get(`533212850919964683`).send("<@"+message.author +"> Các lệnh liên quan đến waifu xin mời thực hiện ở đây")
+	}
 	else if(message.content.indexOf("/") === 0){
 		await message.delete({ timeout: 60000 });
 		await bot.channels.cache.get(`533212850919964683`).send("<@"+message.author +"> Đã xoá tin nhắn tạm thời")
