124045e15ba2fe6f95b4835409f31bed41c9219a
phanthehien150196 <phanthehien150196@gmail.com>
2020-07-28 15:52:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ function getGame(mention) {
 function change_alias(alias) {
     var str = alias;
     str = str.toLowerCase();
+    str = str.replace("gảm","game")
     str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g,"a"); 
     str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g,"e"); 
     str = str.replace(/ì|í|ị|ỉ|ĩ/g,"i"); 
