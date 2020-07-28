e73501700b45355bc0b9c07442a6969e6f0aa670
phanthehien150196 <phanthehien150196@gmail.com>
2020-07-29 02:10:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on("message", async message => {
 		message.delete({ timeout: 20000 }, function(){
 			bot.channels.cache.get(`694785358746877970`).send("<@"+message.author +"> Đã xoá")	
 		}
-	)};
+	)}
 	else if(message.content.indexOf(".") === 0){ console.log(message.content.slice(1).trim())
 	axios.get('https://simsumi.herokuapp.com/api?text='+encodeURI(message.content.slice(1).trim())+'&lang=vi')
       .then( response =>{
