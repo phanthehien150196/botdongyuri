edcbb5c783b502ee091ab388c3c931a615b8228c
phanthehien150196 <phanthehien150196@gmail.com>
2020-07-29 21:23:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ function change_alias(alias) {
     str = str.replace("ga me","game")
     str = str.replace("gie","")
     str = str.trim();
-    str.replace(/\:!?(\S+)\:/gi, '');  
+    str =str.replace(/\:!?(\S+)\:/gi, '');  
     return str;
 }
 bot.login(process.env.token);
