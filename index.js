e59e7c521986a53fc89b03e32ed2b5d067a4b992
phanthehien150196 <phanthehien150196@gmail.com>
2020-07-31 22:14:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,55 @@ const fs = require('fs');
 const Discord = require("discord.js");	
 const axios = require("axios");		
 var bot = new Discord.Client();		
+const exampleEmbed = {
+	color: 0x0099ff,
+	title: 'Some title',
+	url: 'https://discord.js.org',
+	author: {
+		name: 'Some name',
+		icon_url: 'https://i.imgur.com/wSTFkRM.png',
+		url: 'https://discord.js.org',
+	},
+	description: 'Some description here',
+	thumbnail: {
+		url: 'https://i.imgur.com/wSTFkRM.png',
+	},
+	fields: [
+		{
+			name: 'Regular field title',
+			value: 'Some value here',
+		},
+		{
+			name: '\u200b',
+			value: '\u200b',
+			inline: false,
+		},
+		{
+			name: 'Inline field title',
+			value: 'Some value here',
+			inline: true,
+		},
+		{
+			name: 'Inline field title',
+			value: 'Some value here',
+			inline: true,
+		},
+		{
+			name: 'Inline field title',
+			value: 'Some value here',
+			inline: true,
+		},
+	],
+	image: {
+		url: 'https://i.imgur.com/wSTFkRM.png',
+	},
+	timestamp: new Date(),
+	footer: {
+		text: 'Some footer text here',
+		icon_url: 'https://i.imgur.com/wSTFkRM.png',
+	},
+};
+
 bot.on('ready', function(){		  
 	console.log("bot is now online");		
 })		
@@ -19,26 +68,10 @@ bot.on("message", async message => {
 			}
 
 		});
-	if(message.content.toLowerCase().indexOf("//up")!=-1) 
+	if(message.content.toLowerCase().indexOf(".edit")==0) 
 	{
 
-	axios.get('https://apiv2.gofile.io/getServer')
-      .then( res =>{
-        const sv=res.data.data.server
-        console.log(sv)
-        var url = 'https://'+sv+'.gofile.io/upload';
-	var options = {
-    contentType: 'application/json',
-    file: fs.createReadStream(__dirname + "/u.svg")
-	}
-
-	request.post({url: url, formData: options}, function (err, httpResponse, body) {
-	message.channel.send(body)
-  	console.log(body)
-	});
-	})
-
-
+		message.channel.send({ embed: exampleEmbed });
 
 	}
 	else if(message.content.toLowerCase().indexOf("avatar")!=-1&&getUserFromMention(message.content)!=false){
