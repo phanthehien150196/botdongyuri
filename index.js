27203ff338598773a4c3b21bc0f04a5e14f3e153
phanthehien150196 <phanthehien150196@gmail.com>
2020-07-31 22:20:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,9 @@ const axios = require("axios");
 var bot = new Discord.Client();		
 const exampleEmbed = {
 	color: 0x0099ff,
-	title: 'Some title',
-	url: 'https://discord.js.org',
-	author: {
-		name: 'Some name',
-		icon_url: 'https://i.imgur.com/wSTFkRM.png',
-		url: 'https://discord.js.org',
-	},
-	description: 'Some description here',
-	thumbnail: {
-		url: 'https://i.imgur.com/wSTFkRM.png',
-	},
+	title: 'Những điều cần biết',
+	//url: 'https://discord.js.org',
+	
 	fields: [
 		{
 			name: 'Regular field title',
@@ -31,21 +23,9 @@ const exampleEmbed = {
 			value: 'Some value here',
 			inline: true,
 		},
-		{
-			name: 'Inline field title',
-			value: 'Some value here',
-			inline: true,
-		},
-		{
-			name: 'Inline field title',
-			value: 'Some value here',
-			inline: true,
-		},
+		
 	],
-	image: {
-		url: 'https://i.imgur.com/wSTFkRM.png',
-	},
-	timestamp: new Date(),
+	//timestamp: new Date(),
 	footer: {
 		text: 'Some footer text here',
 		icon_url: 'https://i.imgur.com/wSTFkRM.png',
