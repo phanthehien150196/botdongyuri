c81d1324c09538b583eadb97af1257ede7d96903
phanthehien150196 <phanthehien150196@gmail.com>
2020-08-07 22:14:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -96,6 +96,7 @@ bot.on("message", async message => {
 		var str=message.content.toLowerCase()
 		str=str.replace(".download",'').trim();
 		(async function() {
+		msg = await message.channel.send("Đang lấy thông tin chap truyện")
     	const chapter = await new api.Chapter(getId(str), true);
     	var arr=chapter.pages
     	var title=chapter.title
@@ -107,17 +108,18 @@ bot.on("message", async message => {
 		if (!fs.existsSync(dir)){
     	fs.mkdirSync(dir);
 		}
-    console.log("Đang tải")
+    msg.edit("Đang tải ảnh về máy chủ")
     	for(let i = 0; i < arr.length; i++) {
   			await download_image(arr[i], dir+'/'+i+getPage(arr[i]));
 		}
+		msg.edit("Đang nén ảnh tại máy chủ")
     	await zipDirectory(dir, dir+".zip")
     	await fs.readFile('credentials.json', (err, content) => {
   		if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Drive API.
    		authorize(JSON.parse(content), function(token) {
       //console.log("Got Token"); 
-      //console.log(token);
+      msg.edit("Đang upload lên google drive")
       	uploadFile(chap+"_"+title+".zip",message.channel.id,"<@"+message.author +">",token)
       
     	});
