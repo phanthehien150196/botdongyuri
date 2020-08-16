5901f67557c0b3cb692d1e2ad4ed76a8b5e60285
phanthehien150196 <phanthehien150196@gmail.com>
2020-08-16 20:09:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ bot.on('guildMemberAdd', async member => {
     // This is just to simplify the message being sent below (inviter doesn't have a tag property)
     const inviter = bot.users.cache.get(invite.inviter.id);
     // Get the log channel (change to your liking)
-    const logChannel = member.guild.channels.find(channel => channel.name === "testsv");
+    const logChannel = member.guild.channels.cache.find(channel => channel.name === "testsv");
     // A real basic message with the information we need. 
     logChannel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
   });
