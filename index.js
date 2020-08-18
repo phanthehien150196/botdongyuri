12b8c98fd8d3407d7de4a660447a73b94e88a09a
phanthehien150196 <phanthehien150196@gmail.com>
2020-08-18 16:56:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -231,10 +231,11 @@ bot.on("message", async message => {
 		let role = message.guild.roles.cache.find(role => role.name === 'Trap');
     	const mem = message.mentions.members.first();
     	mem.roles.add(role);
-    	//let guild = await message.guild.members.fetch();
-    	let memberCount = message.guild.roles.cache.get("694785358742945816").members.size+1;
+    	message.guild.members.fetch().then(fetchedMembers => {
+    	let memberCount = fetchedMembers.roles.cache.get("694785358742945816").members.size;
     	//message.channel.send(memberCount + " members have this role!");
 		await bot.channels.cache.get(`694785358952660998`).setName(`Rau Cải Đắng (`+memberCount+` thành viên)`)
+    	})
     	}
 	}
 })
