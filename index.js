480602d5faaec62328ad6a735e24f01ae47f38f9
phanthehien150196 <phanthehien150196@gmail.com>
2020-09-01 22:49:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -423,7 +423,7 @@ axios.get(link)
       headers: {
     
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
-      'Referer': 'https://blogtruyen.vn'
+      'Referer': 'https://blogtruyen.com'
       }
       })
     .then(async res => {
