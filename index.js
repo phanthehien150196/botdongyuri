e53f544d0e237775305f217c53cc05fe45c545ac
phanthehien150196 <phanthehien150196@gmail.com>
2020-09-01 10:41:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -682,12 +682,12 @@ function getArrNhentai(data){
         return nodes
 }
 global.progressBar = (value, maxValue) => {
-  size=15
+  size=20
   const percentage = value / maxValue; // Calculate the percentage of the bar
   const progress = Math.round((size * percentage)); // Calculate the number of square caracters to fill the progress side.
   const emptyProgress = size - progress; // Calculate the number of dash caracters to fill the empty progress side.
-
-  const progressText = '▇'.repeat(progress); // Repeat is creating a string with progress * caracters in it
+//▇
+  const progressText = '▋'.repeat(progress); // Repeat is creating a string with progress * caracters in it
   const emptyProgressText = '—'.repeat(emptyProgress); // Repeat is creating a string with empty progress * caracters in it
   const percentageText = Math.round10(percentage * 100,-1) + '%'; // Displaying the percentage of the bar
 
