51ec024b3d50393c6526e5cf52f00641fc4255d3
phanthehien150196 <phanthehien150196@gmail.com>
2020-09-01 21:14:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -54,6 +54,9 @@ const download_blt = (url, image_path) =>
     headers : {
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
       'Referer': 'https://blogtruyen.vn'
+      'sec-fetch-dest': 'image'
+      'sec-fetch-mode': 'no-cors'
+      'sec-fetch-site': 'cross-site'
     }
   }).then(
     response =>
