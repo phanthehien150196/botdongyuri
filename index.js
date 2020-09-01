b1f8352e03b8f41eba8735073e64c0785975f765
phanthehien150196 <phanthehien150196@gmail.com>
2020-09-01 10:51:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -687,8 +687,8 @@ global.progressBar = (value, maxValue) => {
   const progress = Math.round((size * percentage)); // Calculate the number of square caracters to fill the progress side.
   const emptyProgress = size - progress; // Calculate the number of dash caracters to fill the empty progress side.
 //▇
-  const progressText = '▋'.repeat(progress); // Repeat is creating a string with progress * caracters in it
-  const emptyProgressText = '—'.repeat(emptyProgress); // Repeat is creating a string with empty progress * caracters in it
+  const progressText = '▇'.repeat(progress); // Repeat is creating a string with progress * caracters in it
+  const emptyProgressText = '  '.repeat(emptyProgress); // Repeat is creating a string with empty progress * caracters in it
   const percentageText = Math.round10(percentage * 100,-1) + '%'; // Displaying the percentage of the bar
 
   const bar = '```ini\n[' + progressText + emptyProgressText + '] ' + percentageText + '\n```'; // Creating the bar
