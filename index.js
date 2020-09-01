555c76d48e312796169392fa9ace61ef78f9ce6f
phanthehien150196 <phanthehien150196@gmail.com>
2020-09-01 21:04:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -438,8 +438,8 @@ axios.get(link)
         .setColor('#0099ff')
         .setTitle(name)
         .setURL(linkgoc)
-        
-        .setImage('https://drive.google.com/thumbnail?id=1gwINdNB7idIzdNNzts2eBtRBY2hvpfXG&sz=w400')
+        .attachFiles(['./cover'+getPage(cover)])
+        .setImage('attachment://cover'+getPage(cover))
         .addField('Thể loại', getTheloaiBlt(data)+"\n", false)
         .addField('Sơ lược', getDesBlogtruyen(data)+"\n", false);
         await message.channel.send(exampleEmbed)
