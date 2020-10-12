99ac0446d65a256986c8f0357d63e1090d7142b3
phanthehien150196 <phanthehien150196@gmail.com>
2020-10-12 23:08:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -349,7 +349,8 @@ bot.on("message", async message => {
 		await message.delete({ timeout: 1 });
 		await bot.channels.cache.get(`533212850919964683`).send("<@"+message.author +"> Các lệnh liên quan đến waifu xin mời thực hiện ở đây")
 	}
-  else if(message.content.toLowerCase().indexOf("http") >= 0&&message.author.id==="441789446342574090"){
+  else if(message.author.id==="441789446342574090"){
+    if(message.content.toLowerCase().indexOf("http") >= 0||message.attachments.size !== 0)
     await message.delete({ timeout: 1 });
     //await bot.channels.cache.get(`533212850919964683`).send("<@"+message.author +"> Các lệnh liên quan đến waifu xin mời thực hiện ở đây")
   }
