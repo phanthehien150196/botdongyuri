4652c959c41a926b90fdf2323623d77da2670715
phanthehien150196 <phanthehien150196@gmail.com>
2021-01-03 15:00:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -242,6 +242,24 @@ bot.on("message", async message => {
 	//if (message.webhookID) {message.delete({ timeout: 3000 })};
   if(message.author.id=="578560798205673482"&&message.channel.id=="744518226829901866"){
     message.react('👌');
+
+    bot.on('messageReactionAdd', async (reaction, user) => {
+  // When we receive a reaction we check if the reaction is partial or not
+  if (reaction.partial) {
+    // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      
+      return;
+    }
+  }
+  // Now the message has been cached and is fully available
+  console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+  // The reaction is now also fully available and the properties will be reflected accurately:
+  console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+});
+
   }
 	if(!message.author.bot){
 
