b82bf038db9011dd9b1cea360bb7f2f04d093ee1
phanthehien150196 <phanthehien150196@gmail.com>
2021-05-01 19:55:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1035,7 +1035,7 @@ await fs.createReadStream(id+'.zip')
     //await bot.channels.cache.get("694785358952661000").send("im", {files: [id+"/"+arrimg[1]]});
     var chuoi='```css\n'
     var dem=0
-    var arrlist= new Array()
+    //var arrlist= new Array()
     var countfile=fs.readdirSync('./'+id).length
     console.log(countfile)
     await fs.readdirSync('./'+id).forEach(async file => {
@@ -1043,12 +1043,15 @@ await fs.createReadStream(id+'.zip')
       await bot.channels.cache.get("694785358952661000").send("", {files: [id+"/"+file]})
       .then(async img=> {
         console.log(img.attachments.first().url)
-        await arrlist.push(img.attachments.first().url)
-        //await bot.channels.cache.get("694785358952661000").send(chuoi+img.attachments.first().url+'\n```');
-        
-          chuoi=chuoi+img.attachments.first().url+'\n'
-          dem++
-          
+        chuoi=chuoi+img.attachments.first().url+'\n'
+        dem++
+        if(den==countfile)
+        //await arrlist.push(img.attachments.first().url)
+        {
+        bot.channels.cache.get("694785358952661000").send(chuoi+'```');
+        await rimraf('./'+id, function () { console.log('done'); });
+        await fs.unlinkSync(id+'.zip')
+        }
 
       })
       //console.log(file);
@@ -1057,9 +1060,7 @@ await fs.createReadStream(id+'.zip')
 
     });
     //await bot.channels.cache.get("694785358952661000").send(chuoi+'```');
-      await rimraf('./'+id, function () { console.log('done'); });
-      await fs.unlinkSync(id+'.zip')
-      console.log(JSON.parse(arrlist))
+      //console.log(JSON.parse(arrlist))
     
   });
 
